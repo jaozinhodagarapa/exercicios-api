5256001bd63b1dcb3b1b8b4606c79f198abb0ec4
jaozinhodagarapa <joaovictorribeirooliveira49@gmail.com>
2023-08-01 15:14:34 -0300

    aula formrequest

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExercicioUm;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('exercicio/um', [ExercicioUmController::class, 'multiplicar']);
+Route::post('exercicio/um', [ExercicioUm::class, 'multiplicar']);
 
 Route::post('exercicio/dois', [ExercicioDoisController::class, 'retornarMaior']);
 
