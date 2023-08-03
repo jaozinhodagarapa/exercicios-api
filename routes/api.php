eb911ec0377f826bdc284aa3c752b987227c98ae
jaozinhodagarapa <joaovictorribeirooliveira49@gmail.com>
2023-08-03 08:06:27 -0300

    atualizando rota

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\ExercicioCinco;
+use App\Http\Controllers\ExercicioDois;
+use App\Http\Controllers\ExercicioOito;
+use App\Http\Controllers\ExercicioQuatro;
+use App\Http\Controllers\ExercicioSeis;
+use App\Http\Controllers\ExercicioSete;
+use App\Http\Controllers\ExercicioTres;
 use App\Http\Controllers\ExercicioUm;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,18 +24,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('exercicio/um', [ExercicioUm::class, 'multiplicar']);
 
-Route::post('exercicio/dois', [ExercicioDoisController::class, 'retornarMaior']);
+Route::post('exercicio/dois', [ExercicioDois::class, 'retornarMaior']);
 
-Route::post('exercicio/tres', [ExercicioTresController::class, 'mediaAritmetica']);
+Route::post('exercicio/tres', [ExercicioTres::class, 'mediaAritmetica']);
 
-Route::post('exercicio/quatro', [ExercicioQuatrooController::class, 'verificarNumero']);
+Route::post('exercicio/quatro', [ExercicioQuatro::class, 'verificarNumero']);
 
-Route::post('exercicio/cinco', [ExercicioCincoController::class, 'verificacao']);
+Route::post('exercicio/cinco', [ExercicioCinco::class, 'verificacao']);
 
-Route::post('exercicio/seis', [ExercicioSeisController::class, 'idade']);
+Route::post('exercicio/seis', [ExercicioSeis::class, 'idade']);
 
-Route::get('exercicio/sete', [ExercicioSeteController::class, 'exibirNumeros']);
+Route::get('exercicio/sete', [ExercicioSete::class, 'exibirNumeros']);
 
-Route::post('exercicio/oito', [ExercicioOitoController::class, 'tabuada']);
+Route::post('exercicio/oito', [ExercicioOito::class, 'tabuada']);
 
 Route::post('exercicio/nove', [ExercicioNoveController::class, 'divisao']);
